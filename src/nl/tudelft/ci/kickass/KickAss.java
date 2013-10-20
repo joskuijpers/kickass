d7eee0010a042e30365f9b236d10f3322395992b
Jos Kuijpers <jos@kuijpersvof.nl>
2013-10-20 15:31:55 +0200

    Some initial commit.

---
+++ src/nl/tudelft/ci/kickass/KickAss.java
@@ -0,0 +1,9 @@
+package nl.tudelft.ci.kickass;
+
+public class KickAss {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/nl/tudelft/ci/kickass/world/World.java
@@ -0,0 +1,5 @@
+package nl.tudelft.ci.kickass.world;
+
+public class World {
+	
+}
